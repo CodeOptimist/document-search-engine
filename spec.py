3f83e218ef81d3af307827bc442a697af12af915
Christopher Galpin <52485+CodeOptimist@users.noreply.github.com>
2016-09-17 03:10:14 -0600

    Tweaked the way the book spec works yet again, this time to finally and fully  accommodate non-session text.

--- spec.py
+++ spec.py
@@ -6,7 +6,8 @@ from whoosh import index, analysis
 
 
 def clean(_text):
-    text = re.sub(r'[\*\#>]+', '', _text).strip()
+    text = _text.replace(r'\(', '').replace(r'\)', '')
+    text = re.sub(r'[\*\#>]+', '', text).strip()
     text = re.sub(r'[ \xa0\n]+', r' ', text).title()
     text = re.sub(r'\bEsp\b', r'ESP', text)
     return text
@@ -22,12 +23,12 @@ def add_document(writer, d, session, content):
         if title:
             d['part_title'] = "- {}<br />".format(title)
 
-    d['chapter_num'] = re.sub(r'^(Preface|Chapter \d+|Session \w+).*', r'\1', d['chapter'])
-
+    d['chapter_num'] = re.sub(r'^(Chapter \d+|Section \d+|Session \w+).*', r'\1', d['chapter'])
     d['chapter_title'] = ""
-    chapter_title = re.sub(r'^(Preface By Seth|Chapter \d+|Session \w+|Appendix)\s*', r'', d['chapter'])
-    if chapter_title:
-        d['chapter_title'] = "- {}<br />".format(chapter_title)
+    if d['chapter_num'] != d['chapter']:
+        chapter_title = d['chapter'].replace(d['chapter_num'], '', 1).strip()
+        if chapter_title:
+            d['chapter_title'] = "- {}<br />".format(chapter_title)
 
     writer.add_document(**d)
 
@@ -60,31 +61,29 @@ def create_index(indexdir):
         }
 
         last_session_id = None
-        # parts = text.split(book['part_split'])[1:] if 'part_split' in book else [text]
-        parts = book['part_re'].split(text)[:-2]
-        for _part_id, _part in zip(parts[1::2], parts[2::2]):
+        parts = book['part_re'].split(text)[:-1]
+        for _part_id, _part in zip(parts[::2], parts[1::2]):
             part = _part_id + _part
             part_id = clean(_part_id)
-            if part_id not in ("Preface By Seth", "Chapter 1", "Session One", "Chapter 7"):
+            if not book['section_re'].match(_part_id):
                 d['part'] = part_id
                 print(part_id)
 
-            chapters = book['section_re'].split(part)
-            for _chapter_id, _chapter in zip(chapters[1::2], chapters[2::2]):
+            chapters = list(filter(None, book['section_re'].split(part)[1:-1]))
+            for _chapter_id, _chapter in zip(chapters[::2], chapters[1::2]):
                 chapter = _chapter_id + _chapter
                 chapter_id = clean(_chapter_id)
                 d['chapter'] = chapter_id
-
-                if chapter_id == 'Appendix':
-                    del d['part']
                 print(chapter_id)
 
-                sessions = book['session_re'].split(chapter)
-                continues_session = None
-                if chapter_id != 'Appendix':
-                    continues_session = bool(re.search(r'[a-z]', sessions[0])) and last_session_id
-                    if continues_session:
-                        add_document(writer, d, last_session_id, sessions[0])
+                sessions = list(filter(None, book['session_re'].split(chapter)[:-1]))
+                if not sessions:
+                    add_document(writer, d, "", chapter)
+                    continue
+
+                continues_session = bool(re.search(r'[a-z]', sessions[0])) and last_session_id
+                if continues_session:
+                    add_document(writer, d, last_session_id, sessions[0])
 
                 for idx, (_session_id, _session) in enumerate(zip(sessions[1::2], sessions[2::2])):
                     session = _session_id + _session
